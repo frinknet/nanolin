86e26ba5fb7938bb9927a80a1eafaf91343c7fa7
Micon Frink <frink@frinknet.com>
2018-02-14 03:59:06 +0000

    adding release stuff

--- prime/build.sh
+++ prime/build.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 add_file /bin/busybox
-add_file /bin/signify
-add_file /lib/libbsd*
 add_file /etc/version
 add_file /etc/verify.pub
+
+workbox sh
+workroot asp checkout signify
+workbox sh -c eval "
+"
 
 chmod u+s build/bin/busybox
