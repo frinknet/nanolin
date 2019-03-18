d1e9c5c34058e8a5f5da35e0cec1d1f8bb282960
Micon Frink <frink@frinknet.com>
2019-03-18 16:09:01 +0000

    reorganizing the codebase

--- prime/build.sh
+++ prime/build.sh
@@ -1,29 +1,6 @@
 #!/bin/sh
 
-SGFVER=23
-SGFDIR=signify-$SGFVER
-SGFSRC=v23.tar.gz
-
 yes | workbox adduser build
-
-get_url https://github.com/aperezdc/signify/archive/v$SGFVER.tar.gz
-
-tar xzf $SGFSRC
-
-chmod 777 $SGFDIR -R
-
-workrun make -C $SGFDIR \
-	PREFIX=/usr \
-	LTO=1 \
-	MUSL=1 \
-	BUNDLED_LIBBSD_VERIFY_GPG=0 \
-	EXTRA_CFLAGS='-Os -s' \
-	EXTRA_LDFLAGS='-static' \
-	GIT_TAG=''
-
-workrun make -C $SGFDIR \
-	PREFIX=/usr \
-	GIT_TAG='' install
 
 add_file /bin/busybox
 add_file /bin/signify
